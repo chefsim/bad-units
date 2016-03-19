5f7c12ca84d0417ad1f0f4eeed5eaaa65099d432
Andrew Morton <andrew.morton@brightpearl.com>
2016-03-19 11:58:02 +0000

    Start Java examples

---
+++ Java/src/test/java/com/chefsim/badUnits/AppTest.java
@@ -0,0 +1,17 @@
+package com.chefsim.badUnits;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+/**
+ *
+ */
+public class AppTest {
+
+    @Test
+    public void assertAlwaysPasses() {
+        assertThat(true, equalTo(true));
+    }
+
+}
